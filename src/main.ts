98f05de4a280ba5acd1ed004eef4f02400d7bc51
Jesus Daniel Gomez Hernandez <jesus.gomez@globant.com>
2022-07-17 22:57:58 -0500

    cleaned a main file / erased a comments@

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,8 @@ import organizationRoute from './infraestructure/routes/OrganizationRouter';
 import { scopePerRequest } from "awilix-express";
 import container from "./infraestructure/configurations/container";
 
-console.log(process.env.DATABASE_URL);
-
 const app = express();
+const PORT = process.env.PORT ? process.env.PORT : 3000;
 app.use(express.json());
 app.use(scopePerRequest(container))
 
@@ -16,18 +15,9 @@ app.use('/organization', organizationRoute());
 
 AppDataSource.initialize()
     .then(() => {
-        app.listen(3000, () => {
-            console.log("El servidor está inicializado en el puerto 3000");
+        app.listen(PORT, () => {
+            console.log("Server is running on port 3000");
         });
-        // const organizationRepository = AppDataSource.getRepository(OrganizationDBO);
-        // const organizationData = new OrganizationDBO(1, 'Test', 1);
-        // await organizationRepository.save(organizationData);
-        // console.log(`Created organization with id ${organizationData.getIdOrganization}`);
-        
-
-        // const valueforFind = await organizationRepository.find({where: { idOrganization: 1 }});
-        // valueforFind.forEach( (or) => console.log(`Value found ${or.idOrganization} ${or.getName}`));
-        
     })
     .catch( (error: any) => {
         console.log(error);
