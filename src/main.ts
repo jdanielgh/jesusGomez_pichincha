ea2b724dd24944fd51ce7e0fb5b84621b3cfa084
Jesus Daniel Gomez Hernandez <jesus.gomez@globant.com>
2022-07-17 23:04:04 -0500

    added a dotenv library for variables env configuration

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import express  from 'express';
 import organizationRoute from './infraestructure/routes/OrganizationRouter';
 import { scopePerRequest } from "awilix-express";
 import container from "./infraestructure/configurations/container";
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT ? process.env.PORT : 3000;
@@ -16,7 +19,7 @@ app.use('/organization', organizationRoute());
 AppDataSource.initialize()
     .then(() => {
         app.listen(PORT, () => {
-            console.log("Server is running on port 3000");
+            console.log(`Server is running on port ${PORT}`);
         });
     })
     .catch( (error: any) => {
